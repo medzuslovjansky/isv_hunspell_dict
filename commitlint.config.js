ee2e691d9315319b819a9cd6698b3a8a872e3eea
Yaroslav Serhieiev <noomorph@gmail.com>
2022-01-23 15:09:13 +0200

    build(macos): move directory

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const fs = require('fs');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', [
+      ...fs.readdirSync('packages'),
+      'repo',
+    ]],
+  }
+};
