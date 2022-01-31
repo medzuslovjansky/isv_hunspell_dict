c87f85f8ce0c35c00ce96d8db4c1cbe690d09df9
Yaroslav Serhieiev <noomorph@gmail.com>
2022-01-31 09:41:17 +0200

    build(npm): fix including index.* dictionaries

--- packages/npm/scripts/prepare.mjs
+++ packages/npm/scripts/prepare.mjs
@@ -1,27 +1,24 @@
 #!/usr/bin/env zx
 
 async function copyDictionaries(originalFolder, originalName, targetName) {
-  const dictionariesPath = path.join(process.cwd(), originalFolder);
+  const cwd = process.cwd();
+  const dictionariesPath = path.join(cwd, originalFolder);
 
   for (const ext of ['aff', 'dic']) {
     await fs.copyFile(
       path.join(dictionariesPath, `${originalName}.${ext}`),
-      path.join('dict', `${targetName}.${ext}`)
+      path.join(cwd, `${targetName}.${ext}`)
     );
   }
 }
 
 const copyGeneratedDictionary = (src, dest) => copyDictionaries('../hunspell/output/dictionaries', src, dest);
-const copyNpmDictionary = (lang) => copyDictionaries(`node_modules/dictionary-${lang}`, 'index', lang);
 
 await fs.ensureDir('dict');
 await Promise.all([
-  copyGeneratedDictionary('Medzuslovjansky_KomboLatinicaKirilica', 'art-x-interslv'),
-  copyGeneratedDictionary('Medzuslovjansky_Kirilica', 'art-Cyrl-x-interslv'),
-  copyGeneratedDictionary('Medzuslovjansky_LatinicaStandard', 'art-Latn-x-interslv'),
-  copyGeneratedDictionary('Medzuslovjansky_LatinicaEtimologicna', 'art-Latn-x-interslv-etymolog'),
-  // copyNpmDictionary('cs'),
-  // copyNpmDictionary('fr'),
-  // copyNpmDictionary('pl'),
-  // copyNpmDictionary('ru'),
+  copyGeneratedDictionary('Medzuslovjansky_KomboLatinicaKirilica', 'index'),
+  copyGeneratedDictionary('Medzuslovjansky_KomboLatinicaKirilica', 'dict/art-x-interslv'),
+  copyGeneratedDictionary('Medzuslovjansky_Kirilica', 'dict/art-Cyrl-x-interslv'),
+  copyGeneratedDictionary('Medzuslovjansky_LatinicaStandard', 'dict/art-Latn-x-interslv'),
+  copyGeneratedDictionary('Medzuslovjansky_LatinicaEtimologicna', 'dict/art-Latn-x-interslv-etymolog'),
 ]);
